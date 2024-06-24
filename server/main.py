c3c85d9cb0bf3515a68000341588e27ebd996892
TerminalHermit <commit@terminalhermit.dev>
2024-06-24 14:03:52 +0200

    added inxi deps, uuid by mac, fastapi server for restful stat collection

--- server/main.py
+++ server/main.py
@@ -1,22 +1,24 @@
 import os
 import time
+import json
 from typing import Union
 from pydantic import BaseModel
-from fastapi import FastAPI, APIRouter, Request
+from fastapi import FastAPI, APIRouter, Body, Request
 
 
 app = FastAPI()
 router = APIRouter()
 
 
-@router.post("/stat/")
-async def create_entry(request: Request):
+@router.post("/stat/{mac}")
+async def create_entry(mac: str, request: Request):
     directory = 'out/'
-    filename = f"inxi-{int(time.time())}.json"
+    filename = f"inxi--{mac.replace(":","-")}--{int(time.time())}.json"
     content = await request.json()
     file_path = os.path.join(directory, filename)
     os.makedirs(directory, exist_ok=True)
     with open(file_path, 'w') as file:
-        file.write(f"{content}")
+    #file.write(f"{content}")
+        json.dump(content, file, indent=4)
     
 app.include_router(router)
